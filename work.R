68cb3b470f97ca3df7ccee0b5e5706c7575f2acc
andreality <andrea.sweny@gmail.com>
2018-10-08 11:05:07 -0400

    hugo zen theme

--- work.R
+++ work.R
@@ -3,6 +3,10 @@ library(blogdown)
 setwd("/Users/andreasweny/Documents/github_blog")
 # blogdown::install_hugo(force = TRUE)
 # blogdown::new_site()
-# blogdown::install_theme('spf13/hyde')
+blogdown::install_theme('spf13/hyde')
+install_theme("nishanths/cocoa-hugo-theme")
+install_theme("digitalcraftsman/hugo-steam-theme")
+blogdown::install_theme("rakuishi/hugo-zen")
+
 blogdown::serve_site()
 
